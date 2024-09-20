6fa2db551a76a61c81e2dbf7b4887c64d41a7e50
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-20 15:19:55 +0530

    carddesign.js

---
+++ Gamer's World/carddesign.js
@@ -0,0 +1,15 @@
+let slides=document.querySelectorAll('.slide');
+
+slides.forEach(slide =>{
+    slide.addEventListener("click",()=>{
+        removeCurrentActive();
+        slide.classList.add("active");
+    });
+});
+
+function removeCurrentActive()
+{
+    slides.forEach(slide=>{
+        slide.classList.remove("active");
+    })
+}
